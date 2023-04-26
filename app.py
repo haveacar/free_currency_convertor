1325f445a4ff838f76fc10a3751255521a5b0ae4
DanielZh <haveacar.zhovnir@gmail.com>
2023-04-26 18:06:33 +0300

    fixed index.html and app.py

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/home')
-def hello_world():  # put application's code here
-    return render_template("index.html")
+conversion_rates = {
+    'USD': 1.0,
+    'EUR': 0.83,
+    'GBP': 0.72,
+    'JPY': 109.44,
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        from_currency = request.form['from_currency']
+        to_currency = request.form['to_currency']
+        conversion_rate = conversion_rates[to_currency] / conversion_rates[from_currency]
+        converted_amount = amount * conversion_rate
+        return render_template('index.html', converted_amount=converted_amount)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
