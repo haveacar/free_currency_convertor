b5d6a4b71db6fcc930a26092b678e600d2444761
DanielZh <haveacar.zhovnir@gmail.com>
2023-04-26 09:55:07 +0300

    added bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5002)
