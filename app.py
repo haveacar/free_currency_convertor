90f95d8a076dc0e16a56d98a33393c13065d4f4f
DanielZh <haveacar.zhovnir@gmail.com>
2023-04-27 10:52:07 +0300

    added class rates

--- app.py
+++ app.py
@@ -1,23 +1,103 @@
-from flask import Flask, render_template, request
+import os
+from datetime import datetime
+
+import requests as requests
+from flask import Flask, render_template, request, json
+
+# api key and request url
+API_KEY = "c66xOBOerxjgjCqRfbT3MzvIIqMoDm6e"
+REQUEST_URL = "https://api.apilayer.com/fixer/latest?base=USD"
+
+# path
+CURRENT_PATH = os.path.dirname(__file__)
+CURRENT_PATCH_JASON = os.path.join(CURRENT_PATH, "static")
+
+class Currency:
+    '''Currency convertor'''
+    def __init__(self):
+        pass
+
+    def currency_convector(self,f_rates):
+
+        def receive_data():
+            """
+            Receive_data func check
+            :return:  dict(collected_rates) or False
+            """
+            try:
+                # receive data
+                response_data = requests.get(REQUEST_URL, headers={"UserAgent": "XY", "apikey": API_KEY})
+                collected_rates = json.loads(response_data.text)
+                if collected_rates.get("success") == True: return collected_rates
+
+            except:
+                return False
+
+        def reload_rates() -> dict:
+            """
+            Func Reload_data checks date from file
+            if date!= date.now try upload
+            :return: dict (rates), str(date)
+            """
+            time_now = datetime.now().strftime("%Y-%m-%d")
+            # open rates from file
+            with open(os.path.join(CURRENT_PATCH_JASON, "data_rates.json")) as f:
+                rates_from_file = json.load(f)
+            rates = rates_from_file
+
+            # check from file current date
+            if rates_from_file.get("date") != time_now:
+                print("Uploading Data")
+                rates = receive_data()
+
+                # cannot receive data
+                if rates == False:
+                    print("Cannot update")
+                    rates = rates_from_file
+                else:
+                    print("Uploading successful")
+                    # create file
+                    with open(os.path.join(CURRENT_PATCH_JASON, "data_rates.json"), "w") as f:
+                        json.dump(rates, f, indent=4)
+                        pass
+            return rates
+
+        all_rates = reload_rates()
+
+        # get data rates
+        date=all_rates.get("date")
+
+        # create dict favorite rates
+        dict_favorite_rates = dict.fromkeys(f_rates, 0)
+        for next_key in f_rates:
+            dict_favorite_rates[next_key] = all_rates.get("rates").get(next_key)
+
+        # get all rates
+        full_rates = all_rates.get("rates")
+
+
+        return dict_favorite_rates, date, full_rates
+
 
 app = Flask(__name__)
+conversion_rates= Currency()
 
 
-conversion_rates = {
-    'USD': 1.0,
-    'EUR': 0.83,
-    'GBP': 0.72,
-    'JPY': 109.44,
-}
+favorite_rates= ('USD', 'EUR', 'GBP', 'JPY')
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
+
         amount = float(request.form['amount'])
         from_currency = request.form['from_currency']
         to_currency = request.form['to_currency']
-        conversion_rate = conversion_rates[to_currency] / conversion_rates[from_currency]
-        converted_amount = amount * conversion_rate
+        # get conversion rate
+        conversion_rate = conversion_rates.currency_convector(favorite_rates)[0][to_currency] / conversion_rates.currency_convector(favorite_rates)[0][from_currency]
+        converted_amount = round(amount * conversion_rate, 2)
         return render_template('index.html', converted_amount=converted_amount)
     else:
         return render_template('index.html')
